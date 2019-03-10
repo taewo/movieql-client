2d7846cc3ec870d249b265cc5a3c3d3057bb99fd
Taewoong Moon <moondingie5@gmail.com>
2019-03-10 13:50:50 +0900

    graphql 통해 api 받는 작업_2

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,25 @@
 import React from 'react';
-import Query from 'react-apollo';
+import { Query } from 'react-apollo';
 import { HOME_PAGE } from './queries';
 
-const Home = () => <Query query={HOME_PAGE}></Query>
+const Home = () => (
+    <Query query={HOME_PAGE}>
+        {({ loading, data, error }) => {
+            if(loading) return <span>loading</span>
+            if(error) return <span>something happend</span>
+            // if(data) {
+            //     console.log(data)
+            //     return <span>yay!!!</span>
+            // }
+            return data.movies.map((movie) => {
+                return (
+                    <h2 key={movie.id}>
+                        {movie.title} / {movie.rating}
+                    </h2>
+                )
+            })
+        }}
+    </Query>
+)
 
 export default Home;
